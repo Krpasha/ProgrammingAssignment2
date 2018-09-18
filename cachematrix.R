38ed85645a6493ab9270c19871fc7eda34453b83
Krpasha <krpasha@gmail.com>
2018-09-18 10:59:22 +0200

    ProgrammingAssignment2_Commit2

--- cachematrix.R
+++ cachematrix.R
@@ -5,10 +5,10 @@
 # FUNCTION: makeCacheMatrix
 
 ## makeCacheMatrix is a function which is actually a list containing 4 functions.   
-## The first element is the set function, which sets the value of the matrix,
-## the second element is the get funtion, which gets the value of the matrix,
-## the third element is the setsolve function, which computes the inverse of the matrix
-## the fourth element is the getsolve function, which gets the value of inverted matrix
+## The first element is the set function, which sets the value of the matrix "M",
+## the second element is the get funtion, which gets the value of "M",
+## the third element is the setsolve function, which sets the value of the inverse of "M"
+## the fourth element is the getsolve function, which gets the value of inverted "M"
 
 
 makeCacheMatrix <- function(x = matrix()){
@@ -22,18 +22,18 @@ makeCacheMatrix <- function(x = matrix()){
   get <- function()x
   setsolve <- function (solve) M <<- matrix
   getsolve <- function ()M
-  
   list (set = set, 
         get = get,
         setsolve = setsolve,
         getsolve = getsolve)
+        
 }
 
 # FUNCTION: cacheSolve
 
-## cacheSolve is a function that calculates the inverse of the matrix ("x") through 
-## the above function (makeCacheMatrix), and sets the result "M" in the cache, 
-## through the setSolve function.  But it first checks if the inverse was already calculated.
+## cacheSolve is a function that CALCULATES the inverse of the matrix ("x"), 
+## and sets the result in the cache, through the setSolve function of makeCacheMatrix.  
+## But it first checks if the inverse was already calculated and cached in makeCacheMatrix.
 ## if "M" was already calculated, it gets and returns the cached inverse matrix,
 ## skipping the rest of the computation. 
 
